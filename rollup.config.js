14dd5c5b45c159986c335a325cbe24e79ac2dcdd
clipper-dev <96745726+clipper-dev@users.noreply.github.com>
2023-09-24 17:57:09 +0200

    it finally worksgit add .git add .git add .

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import typescript from '@rollup/plugin-typescript'
+import postcss from 'rollup-plugin-postcss'
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+    },
+  ],
+  plugins: [
+    typescript(),
+    postcss({
+      extensions: ['.css'],
+      modules: true,
+    }),
+  ],
+}
